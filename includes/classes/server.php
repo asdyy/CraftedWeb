743cf2bd0a4105ddc824a71bab8731d5305b2bdb
Asdy <asdy.tiby@yahoo.com>
2012-06-28 13:50:07 +0300

    fix some simbols

--- includes/classes/server.php
+++ includes/classes/server.php
@@ -5,7 +5,7 @@
 / /__| | | (_| |  _| ||  __/ (_| |\  /\  /  __/ |_) |
 \____/_|  \__,_|_|  \__\___|\__,_| \/  \/ \___|_.__/ 
 
-		-[ Created by �Nomsoft
+		-[ Created by @Nomsoft
 		  `-[ Original core by Anthony (Aka. CraftedDev)
 
 				-CraftedWeb Generation II-                  
@@ -17,7 +17,7 @@
                   The policy of Nomsoftware states: Releasing our software   
                   or any other files are protected. You cannot re-release    
                   anywhere unless you were given permission.                 
-                  � Nomsoftware 'Nomsoft' 2011-2012. All rights reserved.  */
+                  @ Nomsoftware 'Nomsoft' 2011-2012. All rights reserved.  */
  
 class server {
 	
@@ -103,7 +103,7 @@ class server {
 				  </td>';
 		}
 		
-				//Get uptime
+		//Get uptime
 		if ($GLOBALS['serverStatus']['uptime']==TRUE) 
 		{	
 			connect::selectDB('logondb');			
