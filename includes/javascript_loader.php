43bb3d3fcd9be6aa47ce452b53d044aa705c4b1f
Asdy <asdy.tiby@yahoo.com>
2012-06-28 13:49:24 +0300

    fix some simbols

--- includes/javascript_loader.php
+++ includes/javascript_loader.php
@@ -5,7 +5,7 @@
 #/ /__| | | (_| |  _| ||  __/ (_| |\  /\  /  __/ |_) |
 #\____/_|  \__,_|_|  \__\___|\__,_| \/  \/ \___|_.__/ 
 #
-#		-[ Created by �Nomsoft
+#		-[ Created by @Nomsoft
 #		  `-[ Original core by Anthony (Aka. CraftedDev)
 #
 #				-CraftedWeb Generation II-                  
@@ -17,7 +17,7 @@
 #                  The policy of Nomsoftware states: Releasing our software   
 #                  or any other files are protected. You cannot re-release    
 #                  anywhere unless you were given permission.                 
-#                  � Nomsoftware 'Nomsoft' 2011-2012. All rights reserved.    
+#                  @ Nomsoftware 'Nomsoft' 2011-2012. All rights reserved.    
 
 ?>
 <script type="text/javascript" src="javascript/jquery-1.7.1.min.js"></script>
